0720be43ea84998337927cb035124f6a988ff1b5
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-05 08:42:17 -0300

    13° commit - tdd controller: Consegue listar um fighter especifico e retornar status 200? GREEN

--- app/controllers/api/v1/fighters_controller.rb
+++ app/controllers/api/v1/fighters_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::FightersController < ApplicationController
 
-  #before_action :set_fighter, only: %i[] # show update delete
+  before_action :set_fighter, only: %i[show] # show update delete
 
   def index
     @fighters = Fighter.all 
     render json: @fighters
+  end
+
+  def show
+    render json: @fighter
   end
 
 private
