31a5002ffc6da61dda1830508b8a16ec076f205f
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-05 08:39:45 -0300

    12° commit - tdd controller: Consegue listar um fighter especifico e retornar status 200? RED

--- spec/controllers/fighters_controller_v1_spec.rb
+++ spec/controllers/fighters_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::FightersController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/mk_fighters/id' do
+    it 'Consegue listar um fighter especifico e retornar status 200?' do
+      get :show, params: {id: @fighter.id}
+      expect(response.body).to include_json(id: @fighter.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
