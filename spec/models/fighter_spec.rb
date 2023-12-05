af42dc79f7e58b7a610bee3e6c1910de2c10e901
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-05 07:56:33 -0300

    2° commit - tdd model: name consegue ser preenchido? RED

---
+++ spec/models/fighter_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Fighter, type: :model do
+
+  before{@fighter = Fighter.new}
+
+  describe 'Testes de preenchimento do model Fighter' do
+
+    it 'name consegue ser preenchido?' do
+      @fighter.name = ''
+      expect(@fighter.name).to eq('kabal')
+    end
+
+  end
+
+end
